7476f79fe5206e7d29ea097eeb774164bea5cae1
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-01-24 23:16:15 +0000

    moved folders

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -3,8 +3,8 @@ function config() {
         roi: {"x0": 0, "x1": 9496, "y0": 0, "y1": 7674},
         imageSize: [262144, 211846],
         anchor_tiles: 'dashboard/data/img/262144px_anchor/{z}/{y}/{x}.jpg',
-        cellData: './dashboard/data/tsv/cellData/cellData.json',
-        geneData: './dashboard/data/tsv/geneData/geneData.json',
+        cellData: './dashboard/data/json/cellData/cellData.json',
+        geneData: './dashboard/data/json/geneData/geneData.json',
         cellBoundaries: i => 'http://localhost:63342/baysor_viewer/dashboard/data/json/polys/poly_' + i + '.json',
         seg_names: ['Baysor_1', 'Some Baysor', 'Baysor-3', 'AnoherBaysor 4'],
         seg_colours: ['#009688', '#2944cc', '#ff0101', '#ffc500']
